ad059712abd6a6997d40dc9e8fc3472becf406d7
Simeon <cppsimeonlu@gmail.com>
2022-04-18 15:50:46 +0800

    Init Project.

---
+++ LVFoundation/LVFoundation/LVFoundation.h
@@ -0,0 +1,18 @@
+//
+//  LVFoundation.h
+//  LVFoundation
+//
+//  Created by Shimin lyu on 18/4/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LVFoundation.
+FOUNDATION_EXPORT double LVFoundationVersionNumber;
+
+//! Project version string for LVFoundation.
+FOUNDATION_EXPORT const unsigned char LVFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LVFoundation/PublicHeader.h>
+
+
